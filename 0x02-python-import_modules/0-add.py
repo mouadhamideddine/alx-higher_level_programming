77470cd1c5d836165a7118533cd4dbe3e1ffd354
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-02 21:53:18 +0100

    protect from running

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-c = add(a, b)
-print(f'{a} + {b} = {c}')
+if __name__ == "__main__":
+    
+    a = 1
+    b = 2
+    c = add(a, b)
+    print(f'{a} + {b} = {c}')
