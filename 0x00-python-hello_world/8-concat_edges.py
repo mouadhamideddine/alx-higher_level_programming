d78f7bbe5ef2335e82be223c1ad9018e6013d502
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-03 19:32:18 +0100

    undo comment

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-#1 extra just for you ALX HAHA
 print(str[39:66] + str[114:120] + str[:6])
+
