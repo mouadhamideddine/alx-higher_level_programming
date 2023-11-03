e271791b452862ef1e83cf5e472700ec90e5fd20
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-03 22:34:01 +0100

    check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * traverse_check - traverses a list and checks if it's a cycle list
+ * @head : headnode
+ * Return: 0 if no cycle 1 if cycle
+ */
+int traverse_check(listint_t *head)
+{
+	listint_t *current = head;
+
+	while (current)
+	{
+		current = current->next;
+		if (current == head)
+		{
+			return (1);/*cycle*/
+		}
+	}
+	return (0);
+}
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list : the linked list starting point to check
+ * Return: 0 IF NO CYCLE 1 IF CYCLE
+ */
+int check_cycle(listint_t *list)
+{
+	if (!list)
+	{
+		printf("NULL LIST");
+		return (2);
+	}
+	return (traverse_check(list));
+}
