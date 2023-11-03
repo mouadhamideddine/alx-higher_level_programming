60f163c5493bf57ab5162387788417434481cc67
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-03 22:42:07 +0100

    betty styling lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,13 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * 
- *        */
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
 	int n;
