f845e1368a24b7150e83402933c305659761fe2c
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-03 19:28:38 +0100

    removed blank line from 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
 print(str[39:66] + str[114:120] + str[:6])
-
