2ea0e920d65750693121ad6b1d521a0ae4efa006
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 00:46:56 +0100

    removed protection from being executed if imported as a module

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for number in my_list:
-            print("{:d}".format(number))
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))
 #_list = [1,10]
 #print_list_integer(_list)
