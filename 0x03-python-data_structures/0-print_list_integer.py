9dd58333f3a51d4345b8e14ee0cab927f31904e7
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 00:33:07 +0100

    protection against running when imported

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for number in my_list:
-        print("{}".format(number))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for number in my_list:
+            print("{}".format(number))
+#_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#print_list_integer(_list)
