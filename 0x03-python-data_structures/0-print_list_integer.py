1275b8b858bd73d21d07c104f3ca96e68d725c5d
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 00:28:34 +0100

    just checking

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     for number in my_list:
         print("{}".format(number))
-#_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#print_list_integer(_list)
+_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print_list_integer(_list)
