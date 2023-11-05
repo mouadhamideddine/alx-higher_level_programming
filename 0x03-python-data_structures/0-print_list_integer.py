f4a5a8e40420a5106facc0e66aa179736633eaee
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 00:45:51 +0100

    format specifier

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for number in my_list:
-            print("{}".format(number))
-#_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            print("{:d}".format(number))
+#_list = [1,10]
 #print_list_integer(_list)
