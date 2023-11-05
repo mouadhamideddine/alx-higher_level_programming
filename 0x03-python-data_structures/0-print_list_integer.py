61f1df47958bbc8b3c8c849cee688f0c9a5487de
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 00:47:39 +0100

    pycode style

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     for number in my_list:
         print("{:d}".format(number))
-#_list = [1,10]
-#print_list_integer(_list)
+# _list = [1,10]
+# print_list_integer(_list)
