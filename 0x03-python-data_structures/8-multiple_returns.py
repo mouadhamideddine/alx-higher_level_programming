cb26a843f7e50175cad6e6643506fc73bd6b5bf0
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-07 00:29:25 +0100

    *pycode

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,9 +3,11 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return (0, None)
-    return(len(sentence), sentence[0])
+    return (len(sentence), sentence[0])
 
 
-# sentence = "At school, I learnt C!"
-# length, first = multiple_returns(sentence)
-# print("Length: {:d} - First character: {}".format(length, first))
+'''
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+'''
