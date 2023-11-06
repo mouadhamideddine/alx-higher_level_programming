d0494c59ab874f24bff295a2df7a222b02a24270
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 22:13:43 +0100

    replaced variable character with char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     str_copy = ""
-    for character in my_string:
-        if character != 'c' and character != 'C':
-            str_copy += character
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            str_copy += char
     return str_copy
 # strin = ""
 # print(no_c("Best School"))
