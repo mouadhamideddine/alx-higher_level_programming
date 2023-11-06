75b5517c6e1992d52eb70e655392407a6e7ea919
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-06 22:34:02 +0100

    i give up don't know error here

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,3 +6,11 @@ def no_c(my_string):
         if elements != "c" and elements != "C":
             new_string += elements
     return new_string
+
+
+# print(no_c("Best School"))
+# print(no_c("Chicago"))
+# print(no_c("C is fun!"))
+# input_string = "This is a test string with some Cs and Cs."
+# result = no_c(input_string)
+# print(result)
