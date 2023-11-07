2211b2dd09e686cf6d50801df317f19260b4f6ef
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-07 01:29:53 +0100

    didn't modify original list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    count = 0
+    list_copy = []
+    for element in my_list:
+        if count != idx:
+            list_copy.append(element)
+        count += 1
+    # my_list = list_copy
+    return(my_list)
+'''
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+'''
