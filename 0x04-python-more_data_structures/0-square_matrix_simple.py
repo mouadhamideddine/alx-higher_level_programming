885c6e04b689241f2e18ad43ed20f3a63a2bbbd8
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-08 22:52:58 +0100

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+square = lambda x: x ** 2
+def square_matrix_simple(matrix=[]):
+    squared_nums = []
+    matrix_iter = matrix[:]
+    for _list in matrix_iter:
+        if _list:
+            result = list(map(square, _list))
+            squared_nums.append(result)
+    return squared_nums
+
+'''
+matrix = []
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
+'''
