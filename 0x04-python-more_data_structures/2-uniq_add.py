8a52b8682826192d109df4f3bfa1d7a7ae1065e4
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-11 16:00:45 +0100

    simpler

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    uniq_list = []
-    sum_uniq = 0
+def uniq_add(my_list=[])
     if not my_list:
         return 0
-    for number in my_list:
-        if number not in uniq_list:
-            uniq_list.append(number)
+    uniq_list = set(my_list)
     return sum(uniq_list)
