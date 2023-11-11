c07b16c81e4171cc552d31c43e75f69809df5d19
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-11 23:44:38 +0100

    7-update_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return
     sorted_keys = sorted(a_dictionary.keys())
     sorted_dict = {}
     for sort_key in sorted_keys:
