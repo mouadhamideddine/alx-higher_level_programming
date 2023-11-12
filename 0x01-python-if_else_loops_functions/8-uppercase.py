e872ea9cf7a8ea61b1ba173195f29063f527130a
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-13 00:19:20 +0100

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str_list = list(str)
+    str_index = 0
+    for char in str_list:
+        ascii_char = ord(char)
+        if ascii_char >= 97 and ascii_char <= 122:
+            ascii_char -= 32
+            str_list[str_index] = chr(ascii_char)
+        str_index += 1
+    str_list = ''.join(str_list)
+    print(str_list)
+
+
+'''
+uppercase("best")
+uppercase("Best School 98 Battery street")
+'''
