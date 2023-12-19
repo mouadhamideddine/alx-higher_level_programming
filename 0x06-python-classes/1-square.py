cef388e5f19906d8667fa45696dff18e69e42a18
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-12-19 22:50:09 +0100

    don't know

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,6 +6,6 @@ class Square:
     def __init__(self, size):
         """Constructor.
         Args:
-            size: length of size of the square
+            size: length of size of the square.
         """
         self.__size = size
