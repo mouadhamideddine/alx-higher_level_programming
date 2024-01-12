814bda53837b6de3f0df51fff89345cfa1f134c4
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-01-12 20:22:38 +0100

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,12 +7,20 @@ def add_integer(a, b=98):
     >>> add_integer(5.5, 3.1)
     8
     >>> add_integer("string", 3)
+    Traceback (most recent call last):
+    ...
     TypeError: a must be an integer
     >>> add_integer(4, "string")
-    TypeError: b must be an integer
+    Traceback (most recent call last):
+    ...
+    TypeError: b must be an integere
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
