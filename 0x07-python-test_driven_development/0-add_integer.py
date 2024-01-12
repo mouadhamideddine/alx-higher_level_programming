5880b7f3bf1765890f72a1e2401193a1ca0c9c2d
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-01-12 20:23:38 +0100

    pycode

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,13 +13,14 @@ def add_integer(a, b=98):
     >>> add_integer(4, "string")
     Traceback (most recent call last):
     ...
-    TypeError: b must be an integere
+    TypeError: b must be an integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
