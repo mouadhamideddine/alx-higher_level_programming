77b4653dcf5c6e8b5e3028dbe7fb12a7ed55c919
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-01-16 03:05:40 +0100

    4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" print_square module """
+
+
+def print_square(size):
+    """
+    prints a square
+    @size: size of the square
+    return: nothing
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    # squareprinting
+    for rows in range(size):
+        for lines in range(size):
+            print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
