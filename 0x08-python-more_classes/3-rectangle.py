eafc16ca74bd7ee933f09b42f1ce11cf06788a0b
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-01-23 00:58:39 +0100

    3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+""" Area and perimeter """
+
+
+class Rectangle:
+    """
+    definition of a Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """initializer"""
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        if self.height == 0 or self.width == 0:
+            return ""
+        return ("#" * self.width + "\n") * (self.height - 1) + "#" * self.width
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """returns perimeter of rectangle"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (self.height + self.width) * 2
