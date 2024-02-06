b9b4c50732f1acc51de348eb4b871811d34427cf
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-06 17:58:09 +0100

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,10 @@ read file module
 
 
 def read_file(filename=""):
+    """
+    this function reads a file and prints its content to stdout
+    @filename: string
+    """
     if not filename:
         return
     with open(filename, 'r', encoding='utf8') as openfile:
