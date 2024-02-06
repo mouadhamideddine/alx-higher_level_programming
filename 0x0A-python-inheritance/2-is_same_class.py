9290bf03c4ff1e6e952355528de6aede18dce31a
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-06 03:25:03 +0100

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" is same class module """
+
+
+def is_same_class(obj, a_class):
+    """ is same class function 
+    @obj : object 
+    @a_class : class to check if object is instance
+    return : BOOLEAN
+    """
+    return (isinstance(obj, a_class))
