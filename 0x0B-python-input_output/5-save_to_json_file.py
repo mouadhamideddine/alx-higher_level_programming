8a1c9f30c97bde83529c940bd67de6481ef08734
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-06 18:52:58 +0100

    5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,5 +4,10 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    @my_obj : python object
+    convert my_obj to json string representation
+    and writes it in @filename
+    """
     with open(filename, 'w', encoding='utf8') as f:
         f.write(json.dumps(my_obj))
