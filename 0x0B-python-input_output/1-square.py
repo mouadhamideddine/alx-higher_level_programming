efe5d2cfdb0701305d37515334111797d40a529c
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-07 03:19:16 +0100

    8

---
+++ 0x0B-python-input_output/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """define a square"""
+    def __init__(self, size):
+        """constructor"""
+        self.__size = size
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  Class to JSON module """
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    """
+    return json.dumps(obj.__dict__)
