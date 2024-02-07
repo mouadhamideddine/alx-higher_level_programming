2c8d19a1c4a78db11fbf87f8a5af55eead6b1139
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-07 03:23:23 +0100

    8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """  Class to JSON module"""
-import json
 
 def class_to_json(obj):
     """
     returns the dictionary description with simple data structure
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
