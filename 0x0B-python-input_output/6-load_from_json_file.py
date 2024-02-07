e70771a7bd04bc4277eeb4d7c1f06b477b3580e6
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-07 02:26:15 +0100

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Create object from a JSON file module """
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”
+    @filename : file name or path
+    """
+    with open(filename, 'r', encoding='utf8') as f:
+        return json.loads(f.read())
