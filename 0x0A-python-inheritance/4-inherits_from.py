64c5848009fa6c331732db4d84ef082c5332a8a0
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-08 03:03:47 +0100

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 4. Only sub class of """
+
+
+def inherits_from(obj, a_class):
+    """
+    inherited (directly or indirectly)
+    check
+    """
+    return isinstance(obj, a_class)
