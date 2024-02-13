6cab5a44011efeddc021a0e9e2e697e1c5e1dfd6
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-13 02:14:52 +0100

    Square test

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """sqaure class inherits rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """init square"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """str method"""
+        return f"[square] ({self.id}) {self.x}/{self.y} - {self.size}"
+    
