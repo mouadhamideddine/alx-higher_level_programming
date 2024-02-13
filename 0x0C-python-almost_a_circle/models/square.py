2b9c5dedf56b66e38ce34d1ae830648bdddfcdc9
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-13 02:29:16 +0100

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,10 +9,9 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init square"""
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """Overloading str function"""
         return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
                                              self.id, self.x, self.y,
                                              self.width)
-    
