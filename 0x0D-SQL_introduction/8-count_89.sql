2edc207d79f1974e7515cbe99ff54f8920a12fa3
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-03-12 23:58:19 +0100

    8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,3 @@
 -- a script that displays the number of records with 
 -- id = 89 in the table first_table of the database
-SELECT * FROM first_table
-WHERE id = 89;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
